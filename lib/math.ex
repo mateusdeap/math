ad8ec848d03331519a30ae9104f4ed1d225abd68
Mateus Pereira <mateusdeap@gmail.com>
2022-12-05 10:15:59 -0300

    Adds functions to calculate summations, factorials, powers and cosines

--- lib/math.ex
+++ lib/math.ex
@@ -3,16 +3,117 @@ defmodule Math do
   Documentation for `Math`.
   """
 
+  # For practical uses floats can be considered equal if their difference is less than this value. See <~>.
+  @epsilon 1.0e-15
+
+  # Theoretical limit is 1.80e308, but Erlang errors at that value, so the practical limit is slightly below that one.
+  @max_value 1.79_769_313_486_231_580_793e308
+
+  # Default number of terms for any series expansion
+  @number_of_terms 16
+
+  @type x :: number
+  @type y :: number
+
   @doc """
-  Hello world.
+  The mathematical constant *π* (pi).
+  The ratio of a circle's circumference to its diameter.
+  The returned number is a floating-point approximation (as π is irrational)
+  """
+  @spec pi :: float
+  defdelegate pi, to: :math
+
+  @rad_in_deg 180 / :math.pi()
+
+  @doc """
+  The mathematical constant *ℯ* (e).
+  """
+  @spec e :: float
+  def e, do: 2.718281828459045
+
+  @doc """
+  Equality-test for whether *x* and *y* are _nearly_ equal.
+  This is useful when working with floating-point numbers, as these introduce small rounding errors.
+  ## Examples
+      iex> 2.3 - 0.3 == 2.0
+      false
+      iex> 2.3 - 0.3 <~> 2.0
+      true
+  """
+  @spec number <~> number :: boolean
+  def x <~> y do
+    abs_x = abs(x)
+    abs_y = abs(y)
+    diff = abs(x - y)
+
+    # Hacky comparison for floats that are nearly equal.
+    cond do
+      x == y ->
+        true
+
+      x == 0 or y == 0 ->
+        diff < @epsilon
+
+      true ->
+        diff / min(abs_x + abs_y, @max_value) < @epsilon
+    end
+  end
+
+  @doc """
+  This function should return the cosine value of arg
 
   ## Examples
 
-      iex> Math.hello()
-      :world
+      iex> Math.cosine(0)
+      1.0
 
   """
-  def hello do
-    :world
+  def cosine(arg), do: cosine_taylor_expansion(arg, @number_of_terms)
+  def cosine(arg, precision) do
+    cosine_taylor_expansion(arg, @number_of_terms)
+    |> Float.round(precision)
   end
+
+  defp cosine_taylor_expansion(x, order) do
+    summation(0, order, x, &taylor_cosine_term/2)
+  end
+
+  defp taylor_cosine_term(x, k) do
+    power(-1, k) * power(x, 2*k) / factorial(2*k)
+  end
+
+  @doc """
+  This function has 2 signatures.
+
+  The first one is when we want to do a simple summation. It returns the sum
+  of all terms given by `term_function` between `k0` and `k`.
+
+  The second one is when we want to sum an expansion series which is usually
+  done around a given point x0, it then returns the sum of all the terms
+  between `k0` and `k` at the point `x0` where each term is a function of
+  `x` and `k`, given by `term_function`.
+
+  ## Examples
+  
+  """
+  def summation(k0, k = k0, term_function), do: term_function.(k)
+  def summation(k0, k, term_function) do
+    term_function.(k) + summation(k0, k - 1, term_function)
+  end
+  def summation(k0, k = k0, x0, term_function), do: term_function.(x0, k)
+  def summation(k0, k, x0, term_function) do
+    term_function.(x0, k) + summation(k0, k - 1, x0, term_function)
+  end
+
+  @doc """
+  Returns `x` to the power `pow`
+  """
+  def power(x, pow), do: x ** pow
+
+  @doc """
+  Calculates the factorial of a given number `n`
+  """
+  def factorial(n), do: factorial_of(n, 1)
+  defp factorial_of(0, acc), do: acc
+  defp factorial_of(n, acc) when n > 0, do: factorial_of(n - 1, n * acc)
 end
--- test/math_test.exs
+++ test/math_test.exs
@@ -1,8 +1,30 @@
 defmodule MathTest do
   use ExUnit.Case
+  import Math
   doctest Math
 
-  test "greets the world" do
-    assert Math.hello() == :world
+  test "cosine" do
+    assert Math.cosine(0) <~> 1.0
+    assert Math.cosine(0.5) <~> 0.8775825618903728
+    assert Math.cosine(Math.pi) <~> -1.0
+    assert Math.cosine(Math.pi/2) <~> 0.0
+  end
+
+  test "summation" do
+    # Simple summation
+    term_function = fn j -> j**2 end
+    assert Math.summation(1, 100, term_function) == 338_350
+
+    # Taylor expansion
+    taylor_term = fn x, k -> power(-1, k) * power(x, 2*k) / factorial(2*k) end
+    assert Math.summation(0, 16, 0, taylor_term) <~> 1.0
+  end
+
+  test "power" do
+    assert Math.power(3, 2) == 9
+  end
+
+  test "factorial" do
+    assert Math.factorial(3) == 6
   end
 end
